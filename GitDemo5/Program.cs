d6d7151e5061bd4dc1b7b43102bea2880564f86f
Tom Porter <tomporter62@gmail.com>
2020-07-13 10:51:19 +0100

    Added hello world method and call

--- GitDemo5/Program.cs
+++ GitDemo5/Program.cs
@@ -9,7 +9,10 @@ namespace GitDemo5
             Console.WriteLine("Welcome to this Demo!");
 
             Console.WriteLine(Testing123());
+            HelloWorld();
         }
+
+        private static void HelloWorld() => Console.WriteLine("Hello world!");
 
         static string Testing123() => "I wonder what we'll find here 🙂";
     }
