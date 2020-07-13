12703864e557f5473df66e55f1fad95f99c3bc46
William Millington <WMillington@SpartaGlobal.com>
2020-07-13 11:10:31 +0100

    Add new HelloWill File

---
+++ GitDemo5/HelloWill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitDemo5
+{
+    class HelloWill
+    {
+        public HelloWill()
+        {
+            Console.WriteLine("Hello Will!");
+        }
+    }
+}
