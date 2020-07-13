cff8be222d268a79b3b8816cdb492ec365615fd8
Shwetha21 <shwetha21ashwath@gmail.com>
2020-07-13 11:08:02 +0100

    adding a different class

--- GitDemo5/Program.cs
+++ GitDemo5/Program.cs
@@ -4,13 +4,19 @@ namespace GitDemo5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
             int num = 0;
-            for ( int i = 0; i<10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 num += 1;
             }
+            Console.WriteLine("Welcome to this Demo!");
+
+            Console.WriteLine(Testing123());
         }
+
+        static string Testing123() => "I wonder what we'll find here 🙂";
     }
 }
